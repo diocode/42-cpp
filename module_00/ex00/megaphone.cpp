953230caf8caac817c79729ac00ad6b78fb8ff3c
diogo <diogoemail97@gmail.com>
2023-11-22 17:55:09 +0000

    ex00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tutorial.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digoncal <digoncal@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 11:36:31 by digoncal          #+#    #+#             */
+/*   Updated: 2023/11/08 11:36:31 by digoncal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 0; av[++i];)
+		for (int j = -1; av[i][++j];)
+			std::cout << (char) toupper(av[i][j]);
+	std::cout << std::endl;
+	return 0;
+}
